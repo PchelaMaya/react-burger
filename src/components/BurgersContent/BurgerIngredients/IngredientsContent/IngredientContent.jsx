04196acffd52ac652d116a5d963d293556554de1
PchelaMaya <pchelaamaya@gmail.com>
2024-01-03 17:08:28 +0300

    fix useModal IngredientContent

--- src/components/BurgersContent/BurgerIngredients/IngredientsContent/IngredientContent.jsx
+++ src/components/BurgersContent/BurgerIngredients/IngredientsContent/IngredientContent.jsx
@@ -4,19 +4,16 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ingredientPropType } from "../../../../utils/types";
 import styles from "./IngredientContent.module.scss";
-import { useState } from "react";
 import { IngredientDetails } from "../../../Modals/IngredientDetails/IngredientDetails";
+import { useModal } from "../../../../hooks/useModal";
 import { Modal } from "../../../Modals/Modal/Modal";
 
 export const IngredientContent = (props) => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const { isModalOpen, openModal, closeModal } = useModal();
 
   return (
     <>
-      <div
-        className={styles.ingredientitem}
-        onClick={() => setIsOpenModal(true)}
-      >
+      <div className={styles.ingredientitem} onClick={openModal}>
         <div className={styles.image}>
           <img src={props.data.image} alt={props.data.name} />
           <Counter count={1} />
@@ -26,8 +23,8 @@ export const IngredientContent = (props) => {
         </p>
         <p className="text text_type_main-default">{props.data.name}</p>
       </div>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
           <IngredientDetails data={props.data} />
         </Modal>
       )}
